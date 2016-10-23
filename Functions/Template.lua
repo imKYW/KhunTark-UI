f249ade01b2358d184a60f8907b19deffa23a00a
Kimbanjang <dhejrgn@gmail.com>
2016-10-23 11:20:54 +0900

    tweak Template more need tweak

--- Functions/Template.lua
+++ Functions/Template.lua
@@ -1,18 +1,18 @@
-local name, ns = ...
+local _, ns = ...
 local cfg = ns.cfg
 
-framebd = function(parent, anchor) 
-    local frame = CreateFrame('Frame', nil, parent)
-    frame:SetFrameStrata('BACKGROUND')
-    frame:SetPoint('TOPLEFT', anchor, 'TOPLEFT', -3, 3)
-    frame:SetPoint('BOTTOMRIGHT', anchor, 'BOTTOMRIGHT', 3, -3)
-    frame:SetBackdrop({
-    edgeFile = cfg.glow, edgeSize = 3,
-    bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
-    insets = {left = 3, right = 3, top = 3, bottom = 3}})
-    frame:SetBackdropColor(0, 0, 0)
-    frame:SetBackdropBorderColor(0, 0, 0)
-    return frame
+-- Frame Backdrop -----------------------------------------------------------------------
+local fBackDrop = function(parent, anchor) 
+    local f = CreateFrame('Frame', nil, parent)
+    f:SetFrameLevel(parent:GetFrameLevel()-1 or 0)
+    f:SetPoint('TOPLEFT', anchor, 'TOPLEFT', -3, 3)
+    f:SetPoint('BOTTOMRIGHT', anchor, 'BOTTOMRIGHT', 3, -3)
+    f:SetBackdrop({
+        edgeFile = cfg.glow, edgeSize = 3,
+        bgFile = 'Interface\\ChatFrame\\ChatFrameBackground',
+        insets = {left = 3, right = 3, top = 3, bottom = 3}})
+    f:SetBackdropColor(0, 0, 0)
+    f:SetBackdropBorderColor(0, 0, 0)
 end
 
 -----------------------------------------------------------------------------------------
@@ -22,8 +22,8 @@ local fixStatusbar = function(bar)
     bar:GetStatusBarTexture():SetVertTile(false)
 end
 
-createStatusbar = function(parent, tex, layer, height, width, r, g, b, alpha)
-    local bar = CreateFrame'StatusBar'
+local cStatusbar = function(parent, texture, layer, height, width, r, g, b, alpha)
+    local bar = CreateFrame('StatusBar', nil, parent)
     bar:SetParent(parent)
     if height then
         bar:SetHeight(height)
@@ -31,23 +31,21 @@ createStatusbar = function(parent, tex, layer, height, width, r, g, b, alpha)
     if width then
         bar:SetWidth(width)
     end
-    bar:SetStatusBarTexture(tex, layer)
+    bar:SetStatusBarTexture(texture, layer)
     bar:SetStatusBarColor(r, g, b, alpha)
     fixStatusbar(bar)
-    return bar
 end
 
 -----------------------------------------------------------------------------------------
 
-fs = function(parent, layer, font, fontsiz, outline, r, g, b, justify)
+local cFontString = function(parent, layer, font, fontsize, fontflag, r, g, b, justify)
     local string = parent:CreateFontString(nil, layer)
-    string:SetFont(font, fontsiz, outline)
+    string:SetFont(font, fontsize, fontflag)
     string:SetShadowOffset(cfg.shadowoffsetX, cfg.shadowoffsetY)
     string:SetTextColor(r, g, b)
     if justify then
         string:SetJustifyH(justify)
     end
-    return string
 end
 
 -----------------------------------------------------------------------------------------
